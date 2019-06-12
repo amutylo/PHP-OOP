b1738e34f294751f08d5920bd7a486e13f07b9ba
Andrii Mutylo <amutylo@webcodin.com>
2019-06-12 18:22:03 +0300

    update new folders and files structure

---
+++ src/Entity/AbstractEntity.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Entity;
+
+use DateTime;
+
+abstract class AbstractEntity
+{
+  protected $id;
+
+  protected $dateCreated;
+
+  protected $dateUpdated;
+
+  protected function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function setId(?int $id): AbstractEntity
+  {
+    $this->id = $id;
+    return $this;
+  }
+
+  public function dateCreated(): ?DateTime
+  {
+    return $this->dateCreated;
+  }
+
+  public function dateUpdated(): ?DateTime
+  {
+    return $this->dateUpdated;
+  }
+}
--- src/Repository/Type/Invoice.php
+++ src/Repository/Type/Invoice.php
@@ -5,18 +5,31 @@ namespace Repository\Type;
 use Repository\AbstractRepository;
 
 class Invoice extends AbstractRepository {
+
+  /**
+   *
+   */
   public function save() {
     // TODO: Implement save() method.
   }
 
+  /**
+   * @param \Repository\Type\int $id
+   */
   public function findOne(int $id) {
     // TODO: Implement findOne() method.
   }
 
+  /**
+   * @param array $options
+   */
   public function findAllBy(array $options) {
     // TODO: Implement findAllBy() method.
   }
 
+  /**
+   * @param array $options
+   */
   public function findOneBy(array $options) {
     // TODO: Implement findOneBy() method.
   }
