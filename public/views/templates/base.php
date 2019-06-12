6a36dc4811032fdaab429b5657f520b54d07e636
Andrii Mutylo <amutylo@webcodin.com>
2019-06-12 16:27:30 +0300

    update base

--- public/views/templates/base.php
+++ public/views/templates/base.php
@@ -1,2 +1,18 @@
-<?php
-include_once 'primary-menu.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
+<body>
+  <div id="page">
+    <header>
+      <nav>
+        <?php include_once 'primary-menu.php';?>
+      </nav>
+    </header>
+    <section id="page-content">
+    </section>
+  </div>
+</body>
+</html>
