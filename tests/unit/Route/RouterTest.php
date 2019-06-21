26b6854c955d8461a46e31d0fbc61c88ffc409a7
Andrii Mutylo <amutylo@webcodin.com>
2019-06-21 17:10:19 +0300

    test and mapper

---
+++ tests/unit/Route/RouterTest.php
@@ -0,0 +1,30 @@
+<?php namespace Route;
+
+use Helper\Route\Router;
+
+class RouterTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+  /**
+  *
+  * @group router
+  * @group router-routes-array
+  */
+  public function testIsRoutesAnArray()
+  {
+    $router = new Router();
+    $this->assertIsArray($router->getRoutes());
+  }
+}
