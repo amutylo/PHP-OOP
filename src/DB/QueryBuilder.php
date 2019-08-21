8c8cdb4516114ae50eef6a3613ae5f39d9d343dc
Andrii Mutylo <amutylo@webcodin.com>
2019-08-21 11:27:07 +0300

    update query builder

--- src/DB/QueryBuilder.php
+++ src/DB/QueryBuilder.php
@@ -1,70 +1,73 @@
 <?php
-
 namespace App\DB;
-
 class QueryBuilder
 {
-
-  /**
-   * @param array $data
-   * @param string $table
-   *
-   * @return string
-   */
-  public static function insert(array $data, string $table): string
-  {
-    $sql = 'INSERT INTO `'.$table.'`';
-    $values = 'VALUE (';
-    for ($i = 0; $i < count($data); $i++) {
-      if ($i > 0) {
-        $values .= ",";
-      }
-      $values .= "?";
+    /**
+     * @param array $data
+     * @param string $table
+     * @return string
+     */
+    public static function insert(array $data, string $table): string
+    {
+        $sql = 'INSERT INTO `'.$table.'`';
+        $values = 'VALUE (';
+        for ($i = 0; $i < count($data); $i++) {
+            if ($i > 0) {
+                $values .= ",";
+            }
+            $values .= "?";
+        }
+        $values .= ')';
+        $fields = implode('`, `', array_keys($data));
+        $sql .= " (`" . $fields . "`)" . " " . $values . ";";
+        return $sql;
     }
-    $values .= ')';
-    $fields = implode('`, `', array_keys($data));
-    $sql .= " (`" . $fields . "`)" . " " . $values . ";";
-    return $sql;
-  }
-
-  /**
-   * @param array $data
-   * @param string $table
-   * @param array $where
-   *
-   * @return string
-   */
-  public static function update(array $data, string $table, array $where): string
-  {
-    $whereSQL = '';
-    foreach($where as $key => $value) {
-      $whereSQL.='`'.$key.'` =?';
+    /**
+     * @param array $data
+     * @param string $table
+     * @param array $where
+     * @return string
+     */
+    public static function update(array $data, string $table, array $where): string
+    {
+        $whereSQL = '';
+        foreach($where as $key => $value) {
+            $whereSQL.='`'.$key.'` =?';
+        }
+        $sql = 'UPDATE `'.$table.'` SET ';
+        $fields = implode('` =?, `', array_keys($data));
+        $sql.= "`".$fields."` =?" . " WHERE " . $whereSQL .";";
+        return $sql;
     }
-    $sql = 'UPDATE `'.$table.'` SET ';
-    $fields = implode('` =?, `', array_keys($data));
-    $sql.= "`".$fields."` =?" . " WHERE " . $whereSQL .";";
-    return $sql;
-  }
-
-  /**
-   * @param array $data
-   * @param string $table
-   * @param array $where
-   *
-   * @return string
-   */
-  public static function insertOrUpdate(array $data, string $table, array $where = []): string
-  {
-    if(empty($where)) {
-      $sql = self::insert($data, $table);
-    } else {
-      $sql = self::update($data, $table, $where);
+    /**
+     * @param array $data
+     * @param string $table
+     * @param array $where
+     * @return string
+     */
+    public static function insertOrUpdate(array $data, string $table, array $where = []): string
+    {
+        if(empty($where)) {
+            $sql = self::insert($data, $table);
+        } else {
+            $sql = self::update($data, $table, $where);
+        }
+        return $sql;
     }
-    return $sql;
-  }
-
-  public static function findById(string $table)
-  {
-    return "SELECT * FROM `".$table."` WHERE id=:id";
-  }
+    /**
+     * @param string $table
+     * @return string
+     */
+    public static function findOneBy(string $table)
+    {
+        return "SELECT * FROM `".$table."` WHERE id=:id";
+    }
+    /**
+     * @param string $table
+     * @return string
+     */
+    public static function findAll(string $table)
+    {
+        return "SELECT * FROM `".$table."`";
+    }
 }
